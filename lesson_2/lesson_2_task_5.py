2e65bd2cb10c8346dbe84933b6e3fd74860751f7
ilsinyakov <ilsinyakov@gmail.com>
2024-04-20 17:13:20 +0300

    Fix 5

--- lesson_2/lesson_2_task_5.py
+++ lesson_2/lesson_2_task_5.py
@@ -5,7 +5,9 @@ def month_to_season(number):
         return 'Весна'
     elif number in range(6, 9):
         return 'Лето'
-    else:
+    elif number in range(9, 12):
         return 'Осень'
+    else:
+        return 'Вы ввели несуществующий номер месяца'
 
-print(month_to_season(9))
+print(month_to_season(11))
